c2bb40c1ddf7880f51b6ea2b646d49b19a7f803c
pau <paubotellaguillen30@gmail.com>
2022-06-07 16:58:40 +0200

    Añadir ayuda

--- mover.py
+++ mover.py
@@ -8,9 +8,11 @@ def comando():
     )
     parser.add_argument(
         'origen',
+        help='Fichero o directorio al que le quieres cambiar el nombre, o lo quieres mover'
     )
     parser.add_argument(
-        'destino',
+        'final',
+        help='Nombre nuevo del fichero/directorio o directorio donde se va a mover el origen por el primer argumento'
     )
     return parser.parse_args()
 
